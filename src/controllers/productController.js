bfd2057499543762389005a41135c0801f3bcdfb
jhonnaa <jonnyemeni@gmail.com>
2021-12-16 12:20:54 -0300

    terminando las rutas

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -14,13 +14,48 @@ const productController = {
         res.render('./products/productCart', {title: "Carrito"})
     },
     productDetail: (req, res) => {
-        let juegoUnico = juegos.find(juego => juego.id == req.params.juegoId)
+        let juegoUnico = juegos.find(juego => juego.id == req.params.id)
 
         res.render('./products/productDetail', {title: "Detalles", juego: juegoUnico})
     },
     editProduct: (req, res) => {
         res.render('./products/editProduct', {title: "Editar producto"})
-    }
+    },
+    store: (req, res) => {
+		let nuevoJuego = {
+			nombre : req.body.nombre,
+			descripcion : req.body.descripcion,
+			precio : req.body.precio,
+			edicion : req.body.edicion,      
+			img: req.body.img,
+			categoria : req.body.categoria
+		}
+		productModel.create(nuevoJuego)
+		res.redirect('/')
+	},
+    update: (req, res) => {
+		let productToUpdate = productModel.find(req.params.id)
+		let objetoAct ={
+			id : productToUpdate.id,
+			nombre : req.body.nombre,
+			descripcion : req.body.descripcion,
+			precio : req.body.precio,
+			edicion : req.body.edicion,      
+			img: productToUpdate.img,
+			categoria : req.body.categoria
+        }
+
+		productModel.update(objetoAct)
+		res.redirect('/')
+			
+	},
+    destroy : (req, res) => {
+		productModel.delete(req.params.id)
+		
+		res.redirect('/')
+		
+	}
+
 }
 
 module.exports = productController
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -9,12 +9,16 @@ router.get('/', productController.index);
 /* Con readDetail - LEE PRODUCTO SEGUN ID */
 router.get('/products', productController.products)
 
-router.get('/productDetail/:juegoId', productController.productDetail);
+router.get('/products/:id', productController.productDetail);
+router.post('/products/:id', productController.store);
 
-router.get('/createProduct',productController.createProduct);
+router.get('/products/create',productController.createProduct);
 
 router.get('/productCart', productController.productCart)
 
-router.get("/editProduct", productController.editProduct);
+router.get("/products/:id/edit", productController.editProduct);
+router.put("/products/:id/edit", productController.update);
+
+router.delete("/products/delete/:id", productController.destroy)
 
 module.exports = router
